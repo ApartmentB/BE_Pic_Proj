3645e290d0d5c8ae8ece2a71c9fd13c6574d9f44
Tony Nguyen <tonyviet87@gmail.com>
2016-04-06 16:47:29 -0400

    Made changes to application and registrations controller.. Fixed database.yml.. Changed password column to password_digest in User model

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,3 +1,8 @@
 class RegistrationsController < ApplicationController
-
+	def create
+		@user = User.new(user_name: params["user_name"],
+										 full_name: params["full_name"],
+										 email: params["email"],
+										 password: params["password"])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,27 @@
 class User < ActiveRecord::Base
-  #has_many :posts
-  #has_many :guesses
+ # has_many :posts
+ # has_many :guesses
+ has_secure_password
+
+  validates :email, presence: true, uniqueness: true,
+    format: {
+      with: /.+\@.+\..+/,
+      message: "must have an @ symbol and period"
+    }
+  validates :auth_token, presence: true
+
+
+  def ensure_auth_token
+    unless self.auth_token
+      self.auth_token = User.generate_token
+    end
+  end
+
+  def self.generate_token
+    token = SecureRandom.hex
+    while User.exists?(auth_token: token)
+      token = SecureRandom.hex
+    end
+    token
+  end
 end

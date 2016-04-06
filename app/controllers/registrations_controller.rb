0ec4797ab72fdc2c0f2cba6abe7526228e8d0e91
Tony Nguyen <tonyviet87@gmail.com>
2016-04-06 16:57:34 -0400

    Added create method in registration model

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,16 @@
 class RegistrationsController < ApplicationController
-	def create
-		@user = User.new(user_name: params["user_name"],
-										 full_name: params["full_name"],
-										 email: params["email"],
-										 password: params["password"])
+  def create
+    @user = User.new(user_name: params["user_name"],
+                     full_name: params["full_name"],
+                     email: params["email"],
+                     password: params["password"])
+    @user.ensure_auth_token
+    if @user.save
+      render json: { user: @user.as_json(only: [:full_name, :email, :auth_token ]) },
+             status: :created
+    else
+      render json: { errors: @user.errors.full_messages },
+             status: :unprocessable_entity
+    end
   end
 end

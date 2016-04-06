e96661e381b23efee127ecbe75c3205548c3ea8e
Tony Nguyen <tonyviet87@gmail.com>
2016-04-06 17:00:53 -0400

    add current_user method in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+  
+  def current_user
+    token = request.headers["Auth-Token"]
+    if token
+      User.find_by(auth_token: token)
+    end
+  end
+
 end

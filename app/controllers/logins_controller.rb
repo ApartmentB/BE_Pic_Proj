a658842d40d529c38db49156281ea13d960aa704
Pat McAleavey <pmcaleavey8@gmail.com>
2016-04-07 11:46:33 -0400

    updated logins_controller

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,10 +1,10 @@
 class LoginsController < ApplicationController
 
   def create
-    @user = User.find_by!(email: params["email"])
+    @user = User.find_by!(user_name: params["user_name"])
     if @user.authenticate(params["password"])
       #login[user_id] = @user.id ??
-      render json: { user: @user.as_json(only: [:email]) } #:access_token
+      render json: { user: @user.as_json(only: [:user_name]) } #:access_token
              status: :ok
     else
       render json: { message: "Email or password could not be found." }

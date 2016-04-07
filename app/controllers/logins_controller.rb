9c108aea0c21588744e2c56c9d0313ad44c9757f
Pat McAleavey <pmcaleavey8@gmail.com>
2016-04-07 11:42:50 -0400

    logins controller and routes updated

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,13 +1,15 @@
 class LoginsController < ApplicationController
 
   def create
-    @user = User.find_by!(user_name: params[:user_name], password: params[:password])
-    if @user.authenticate(params[:password])
-      render json: { @user.as_json(only: [email: :access_token]) }
-          status :ok
+    @user = User.find_by!(email: params["email"])
+    if @user.authenticate(params["password"])
+      #login[user_id] = @user.id ??
+      render json: { user: @user.as_json(only: [:email]) } #:access_token
+             status: :ok
     else
-      render json: { :message "Email or password could not be found"}
-          status :unauthorized
+      render json: { message: "Email or password could not be found." }
+             status: :unauthorized
+    end
   end
 
 end

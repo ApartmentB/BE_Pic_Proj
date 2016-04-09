317ac59391eecaa72279b0b0ef32c3fe27906dfd
Pat McAleavey <pmcaleavey8@gmail.com>
2016-04-09 12:30:39 -0400

    give error message if username/password don't match, updated user moded with regex to give errors if try to register users with whitespace and set guidelines

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -2,15 +2,18 @@ class LoginsController < ApplicationController
 
   def create
 
-    @user = User.find_by!(user_name: params["user_name"])#getting from fe
-    if @user.authenticate(params["password"])
+    @user = User.find_by(user_name: params["user_name"])#getting from fe
+    # if @user == nil
+    #   render json: { message: "Username or password could not be found." },
+    #          status: :unauthorized
+    if @user && @user.authenticate(params["password"])#@user can't be nil
 
       render "create.json.jbuilder", status: :created
 
        # render json: { user: @user.as_json(only: [:user_name, :access_token]) },#sending to fe
         #      status: :ok
     else
-      render json: { message: "Email or password could not be found." },
+      render json: { message: "Username or password could not be found." },
              status: :unauthorized
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ActiveRecord::Base
  has_many :posts
- # has_many :guesses
  has_secure_password
+
+  validates :full_name, :user_name, presence: true, uniqueness: true,
+    format: {
+      with: /\A\S{4,8}\z/,
+      message: "must be between 4-8 characters"
+    }
+
 
   validates :email, presence: true, uniqueness: true,
     format: {

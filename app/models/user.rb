50ed4b73d9cf9695330e4b6c0ea8a59cf88377b3
Pat McAleavey <pmcaleavey8@gmail.com>
2016-04-09 12:58:59 -0400

    fixed full_name duplication error

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,17 @@ class User < ActiveRecord::Base
  has_many :posts
  has_secure_password
 
-  validates :full_name, :user_name, presence: true, uniqueness: true,
+  validates :user_name, presence: true, uniqueness: true,
     format: {
       with: /\A\S{4,8}\z/,
       message: "must be between 4-8 characters"
     }
+
+    validates :full_name, presence: true,
+      format: {
+        with: /\A\S{4,8}\z/,
+        message: "must be between 4-8 characters"
+      }
 
 
   validates :email, presence: true, uniqueness: true,

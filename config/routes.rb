07bbe4499e39c160fcafc8685717b3af4a6a267b
Pat McAleavey <pmcaleavey8@gmail.com>
2016-04-10 12:11:29 -0400

    updated post jbuilder and added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-
   post "register", to: "registrations#create", as: "register"
 
   post "login", to: "logins#create", as: "login"
